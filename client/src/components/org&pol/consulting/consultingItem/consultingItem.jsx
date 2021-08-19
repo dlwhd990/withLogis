c6030bd6d53bacdf1b115ef9c33806bfa8244749
JONG <dlwhd990@gmail.com>
2021-08-19 16:22:30 +0900

    consulting page update (image part)

--- client/src/components/org&pol/consulting/consultingItem/consultingItem.jsx
+++ client/src/components/org&pol/consulting/consultingItem/consultingItem.jsx
@@ -8,18 +8,20 @@ const ConsultingItem = ({ item }) => {
   return (
     <section className={styles.consulting_item}>
       <div className={styles.item}>
-        <img
-          src={item.image}
-          alt="consulting_image"
-          className={styles.item_head}
-        />
+        <div className={styles.item_head_container}>
+          <img
+            src={`/images/consulting/${item.id}.jpg`}
+            alt="consulting_image"
+            className={styles.item_head}
+          />
+        </div>
+
         <div className={styles.border_maker}></div>
         <div className={styles.item_body}>
-          <p className={styles.name}>{item.name}</p>
-          <p className={styles.desc}>{item.desc}</p>
-          <p className={styles.go_consulting} onClick={goConsulting}>
+          <p className={styles.name}>{item.org_name}</p>
+          <span className={styles.go_consulting} onClick={goConsulting}>
             상담 바로가기
-          </p>
+          </span>
         </div>
       </div>
     </section>
